4015a9652886af9674c3a3b00511fa5d81a37f14
Kevin Whittaker <whi15032@byui.edu>
2017-07-10 09:55:18 -0600

    HW
    Star Wars json retriever

---
+++ homework/API/request.js
@@ -0,0 +1,29 @@
+function getJSON(url) {
+	if (url !== null) {
+		return fetch(url).then(function (response) {
+			return response.json();
+		}).catch(function (error) {
+			window.alert(error);
+		});
+	}
+}
+//lets get some ships
+function fetchShips() {
+	for (var i = 1; i < 50; i++) {
+		var url = "http://swapi.co/api/starships/" + i + "/";
+		//call getJSON function to get the list of ships from the api
+		getJSON(url).then(function (json) {
+			for (var property in json) {
+				if (property == "url") {
+					var link = json[property].replace(json[property], "<a href='$&' target='_blank'>" + json[property] + "</a> &#8594; " + json.name + "<br>" + "<br>");
+					document.getElementById("ships").innerHTML += link;
+				}
+			}
+		})
+	}
+}
+
+function getData() {
+	var url = json[proper]
+	getJSON(url).then(function (json) {})
+}
